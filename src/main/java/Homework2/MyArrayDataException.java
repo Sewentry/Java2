f8a0b53ea74bdb6dce72fc26e66dd5ca9ffc96ac
Sergey <forbic@list.ru>
2022-02-17 22:08:47 +0300

    Homework2-1

--- src/main/java/Homework2/MyArrayDataException.java
+++ src/main/java/Homework2/MyArrayDataException.java
@@ -2,7 +2,7 @@ package Homework2;
 
 public class MyArrayDataException extends RuntimeException{
     public MyArrayDataException (){
-        super("array length exceeded");
+        super("Symbol can't change to int");
     }
 
 }
